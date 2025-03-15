699eacbefa9512b41ebd0675689e4760a83698c6
blackjacket1403 <1403.nc.photos@gmail.com>
2025-03-16 01:57:43 +0530

    Delete hospitalbot_pathplanning directory

--- hospitalbot_pathplanning/build/hospital_robot_spawner/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/nishant/Desktop/Projects/Navigation_/hospitalbot_pathplanning/install/hospital_robot_spawner'
